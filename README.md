bd1eb3e3a5bb3cd535f6c5307e36bdaf8e2d87e8
Mrkazik99 <mateuszkazmierczak99@gmail.com>
2021-03-29 19:24:12 +0200

    rebuild, v0.5

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Simply use: @[name_of_bot] [some_title]
 
 then wait for results and tap one you are looking for.
 
+Special thanks to [@loboda4450](https://github.com/loboda4450/) for async youtube results
+
 SS:
 
 ![Telegram desktop SS](https://i.imgur.com/nwH4Rm3.png)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 import yaml
-import googleapiclient.discovery
+import io
+import aiohttp as aiohttp
+import urllib
 import re
 import requests
 import json
@@ -10,39 +12,38 @@ from mutagen.easyid3 import EasyID3
 from mutagen.flac import Picture
 from mutagen.id3 import ID3, APIC
 from mutagen import id3
+import ffmpeg
+from mutagen.mp3 import MP3
+from pytube import YouTube
 from telethon import TelegramClient, events
 from telethon.tl.types import InputWebDocument
 
-with open("login.yml", 'r') as f:
+with open('login.yml', 'r') as f:
     config = yaml.safe_load(f)
 
-api_service_name = "youtube"
-api_version = "v3"
-DEVELOPER_KEY = config['youtube_apikey']
-
-youtube = googleapiclient.discovery.build(
-    api_service_name, api_version, developerKey=DEVELOPER_KEY)
+api_key = config['youtube_apikey']
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
 client = TelegramClient(**config['telethon_settings']).start(bot_token=config['bot_token'])
 
+session = aiohttp.ClientSession()
 
-async def youtube_search(search_query):
-    if search_query == '' or search_query == ' ':
+f.close()
+
+
+async def youtube_search(type, search_query):
+    search_query = search_query.strip()
+    if not search_query:
         return
-    request = youtube.search().list(
-        part="snippet",
-        maxResults=10,
-        q=search_query
-    )
-    response = request.execute()
-    result = json.dumps(response)
-    result = json.loads(result)
-    result = json.dumps(result["items"])
-    result = json.loads(result)
-    return result
+    async with session.get('https://www.googleapis.com/youtube/v3/search', params={'part': 'snippet',
+                                                                                   'q': search_query,
+                                                                                   'maxResult': 10,
+                                                                                   'type': type,
+                                                                                   'key': api_key}) as response:
+        resp = await response.json()
+        return resp['items']
 
 
 @client.on(events.InlineQuery)
@@ -53,23 +54,27 @@ async def inline_query(event):
         await event.answer([ans])
         return
     else:
-        results = await youtube_search(event.text)
-        ans = []
-        for entry in results:
-            ids = json.dumps(entry['id'])
-            ids = json.loads(ids)
-            if ids['kind'] != 'youtube#channel':
-                snippet = json.dumps(entry['snippet'])
-                snippet = json.loads(snippet)
-                thumb = json.dumps(snippet['thumbnails'])
-                thumb = json.loads(thumb)
-                thumb = json.dumps(thumb['high'])
-                thumb = json.loads(thumb)
-                ans.append(await builder.article(snippet['title'], description=snippet['channelTitle'],
-                                                 thumb=InputWebDocument(url=thumb['url'], size=0, mime_type="image/jpeg",
-                                                                        attributes=[], ),
-                                                 text='https://www.youtube.com/watch?v=' + ids['videoId']))
-        await event.answer(ans)
+        if event.text.startswith('.p'):
+            results = await youtube_search(type='playlist', search_query=event.text[2:])
+        elif event.text.startswith('.v'):
+            results = await youtube_search(type='video', search_query=event.text[2:])
+        else:
+            results = await youtube_search(type='video', search_query=event.text)
+
+    # ans = []
+    # if not results:
+    #     ans.append(await builder.article('No videos found for this query.', text='Empty result'))
+    # else:
+    #     for entry in results:
+    #         if entry['id']['kind'] == 'youtube#video':
+    #             ans.append(
+    #                 await builder.article(entry['snippet']['title'], description=entry['snippet']['channelTitle'],
+    #                                       thumb=InputWebDocument(url=entry['snippet']['thumbnails']['high']['url'],
+    #                                                              size=0,
+    #                                                              mime_type="image/jpeg",
+    #                                                              attributes=[], ),
+    #                                       text='https://www.youtube.com/watch?v=' + entry['id']['videoId']))
+    # await event.answer(ans)
 
 
 @client.on(events.NewMessage)
@@ -77,66 +82,97 @@ async def answer(event):
     if event.text == '/start':
         await event.reply('Siema mały kurwiu ;)')
     if event.text.startswith('@'):
-        await event.respond('It\'s me 😂')
-    if re.match('https://www.youtube.com/watch\?v=(.{11})', event.text):
+        await event.respond("It's me 😂")
+    # if re.match('https://www.youtube.com/watch\?v=(.{11})', event.text):
+    if 'https://www.youtube.com/watch?v=' in event.text or 'https://youtu.be/' in event.text:
         messId = await event.reply("Downloading mp4")
-        ydl_opts = {
-            'format': 'best[ext=mp4]',
-            'keepvideo': True
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info("{}".format(event.text))
-            title = ydl.prepare_filename(info)
-            await client.edit_message(messId, 'Converting to mp3')
-            mp4_file = r'' + title
-            mp3_file = r'' + title + '.mp3'
-            videoclip = VideoFileClip(mp4_file)
-            audioclip = videoclip.audio
-            audioclip.write_audiofile(mp3_file)
-            request = youtube.search().list(
-                part="snippet",
-                maxResults=1,
-                q=info['id']
-            )
-            response = request.execute()
-            result = json.dumps(response)
-            result = json.loads(result)
-            result = json.dumps(result["items"])
-            result = json.loads(result)
-            for entry in result:
-                snippet = json.dumps(entry['snippet'])
-                snippet = json.loads(snippet)
-                thumb = json.dumps(snippet['thumbnails'])
-                thumb = json.loads(thumb)
-                thumb = json.dumps(thumb['high'])
-                thumb = json.loads(thumb)
-            audio = EasyID3(title + '.mp3')
-            audio['title'] = '!' + snippet['title']
-            audio['artist'] = snippet['channelTitle']
-            audio.save()
-            audio = ID3(title + '.mp3')
-            pic = Picture()
-            pic.type = id3.PictureType.COVER_FRONT
-            pic.width = 640
-            pic.height = 640
-            pic.mime = 'image/jpeg'
-            r = requests.get(thumb['url'], stream=True)
-            r.raw.decode_content = True
-            pic.data = r.raw.read()
 
-            audio['APIC'] = APIC(
-                encoding=3,
-                mime='image/jpeg',
-                type=3, desc=u'Cover',
-                data=pic.data
-            )
-            audio.save()
-            videoclip.close()
-            os.remove(title)
-            await client.edit_message(messId, 'Uploading...')
-            await event.reply(file=mp3_file)
-            await client.edit_message(messId, 'Here is your mp3 ⬇️')
-        os.remove(title + '.mp3')
+        yt = YouTube(event.text)
+        print('Downloading')
+        stream = yt.streams.get_audio_only()
+        video = io.BytesIO()
+        stream.stream_to_buffer(buffer=video)
+        print('Converting')
+        file = io.BytesIO(video.getvalue())
+        file.name = 'music.mp4'
+        # out, err = (
+        #     ffmpeg
+        #     .input('pipe:')
+        #     .output('-', format='f32le', acodec='pcm_f32le', ac=1, ar='44100')
+        #     .run(pipe_stdin=True, capture_stdout=True, capture_stderr=True)
+        # )
+        # out.communicate(input=file)
+        process = (
+            ffmpeg
+                .input('pipe:')
+                .output('pipe:')
+                .overwrite_output()
+                .run_async(pipe_stdin=True, pipe_stdout=True, pipe_stderr=True)
+        )
+        out, err = process.communicate(input=file)
+        # print(out)
+        # mp3 = io.BytesIO()
+        # mp3.name = 'music.mp3'
+        print('Uploading')
+        await client.send_file(event.sender.id, file=file)
+
+    # ydl_opts = {
+    #     'format': 'best[ext=mp4]',
+    #     'keepvideo': True
+    # }
+    # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    #     info = ydl.extract_info("{}".format(event.text))
+    #     title = ydl.prepare_filename(info)
+    #     await client.edit_message(messId, 'Converting to mp3')
+    #     mp4_file = r'' + title
+    #     mp3_file = r'' + title + '.mp3'
+    #     videoclip = VideoFileClip(mp4_file)
+    #     audioclip = videoclip.audio
+    #     audioclip.write_audiofile(mp3_file)
+    #     request = youtube.search().list(
+    #         part="snippet",
+    #         maxResults=1,
+    #         q=info['id']
+    #     )
+    #     response = request.execute()
+    #     result = json.dumps(response)
+    #     result = json.loads(result)
+    #     result = json.dumps(result["items"])
+    #     result = json.loads(result)
+    #     for entry in result:
+    #         snippet = json.dumps(entry['snippet'])
+    #         snippet = json.loads(snippet)
+    #         thumb = json.dumps(snippet['thumbnails'])
+    #         thumb = json.loads(thumb)
+    #         thumb = json.dumps(thumb['high'])
+    #         thumb = json.loads(thumb)
+    #     audio = EasyID3(title + '.mp3')
+    #     audio['title'] = '!' + snippet['title']
+    #     audio['artist'] = snippet['channelTitle']
+    #     audio.save()
+    #     audio = ID3(title + '.mp3')
+    #     pic = Picture()
+    #     pic.type = id3.PictureType.COVER_FRONT
+    #     pic.width = 640
+    #     pic.height = 640
+    #     pic.mime = 'image/jpeg'
+    #     r = requests.get(thumb['url'], stream=True)
+    #     r.raw.decode_content = True
+    #     pic.data = r.raw.read()
+    #
+    #     audio['APIC'] = APIC(
+    #         encoding=3,
+    #         mime='image/jpeg',
+    #         type=3, desc=u'Cover',
+    #         data=pic.data
+    #     )
+    #     audio.save()
+    #     videoclip.close()
+    #     os.remove(title)
+    #     await client.edit_message(messId, 'Uploading...')
+    #     await event.reply(file=mp3_file)
+    #     await client.edit_message(messId, 'Here is your mp3 ⬇️')
+    # os.remove(title + '.mp3')
 
 
 with client:
