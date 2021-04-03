19e7510ba669382b2858fa987f487d6ed3288e09
Mrkazik99 <mateuszkazmierczak99@gmail.com>
2021-04-03 19:01:14 +0200

    Working and stable version v0.8

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Youtube-ripper
-Some telegram bot that downloads YouTube video and convert to MP3 with inline search
+Some telegram bot that downloads YouTube video and convert to opus with inline search
 
 Simply use: @[name_of_bot] [.p/.v] [some_title]
 
@@ -12,10 +12,3 @@ SS:
 ![Telegram desktop SS](https://i.imgur.com/nwH4Rm3.png)
 ![Telegram mobile SS](https://i.imgur.com/Zby4JH9.jpg)
 
-TODO:
-
-- download files to ram
-
-- create own downloading script
-
-- ~~add some meta tags to mp3 file~~ //done
--- main.py
+++ main.py
@@ -1,30 +1,20 @@
 import logging
+import ffmpeg as ffmpeg
 import yaml
 import io
 import aiohttp as aiohttp
-# import urllib
-# import re
-# import requests
-# import json
-# import youtube_dl
-# from moviepy.editor import *
-# from mutagen.easyid3 import EasyID3
-# from mutagen.flac import Picture
-# from mutagen.id3 import ID3, APIC
-# from mutagen import id3
-# import ffmpeg
-# from mutagen.mp3 import MP3
+from mutagen import oggopus
 from pytube import YouTube, Playlist, exceptions
 from telethon import TelegramClient, events
 from telethon.tl.types import InputWebDocument
 
-with open('login.yml', 'r') as f:
+with open('config.yml', 'r') as f:
     config = yaml.safe_load(f)
 
 api_key = config['youtube_apikey']
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.DEBUG)
+                    level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 client = TelegramClient(**config['telethon_settings']).start(bot_token=config['bot_token'])
@@ -60,8 +50,22 @@ async def yt_download(event: events.newmessage.NewMessage.Event, yt: YouTube) ->
     yt.streams.get_audio_only(subtype=type_).stream_to_buffer(buffer=video)
     file = io.BytesIO(video.getvalue())
     file.name = f'{yt.title}.{type_}'
+    process = (
+        ffmpeg
+            .input('pipe:')
+            .output('pipe:', format='opus')
+            .run_async(pipe_stdin=True, pipe_stdout=True, pipe_stderr=True)
+    )
+    out, err = process.communicate(input=file.read())
+    out_file = io.BytesIO(out)
+    mutagen_file = oggopus.OggOpus(fileobj=out_file)
+    mutagen_file['artist'] = yt.author
+    mutagen_file['title'] = f'!{yt.title}'
+    mutagen_file.save(out_file)
+    out_file = io.BytesIO(out_file.getvalue())
+    out_file.name = f'{yt.title}.opus'
     await client.edit_message(mess_id, f'__Uploading {yt.title}__')
-    await event.reply(file=file)
+    await event.reply(file=out_file)
     await client.delete_messages(event.sender_id, mess_id)
 
 
@@ -103,26 +107,11 @@ async def inline_query(event):
                                                       text=f"https://www.youtube.com/watch?v={item['id']['videoId']}")
                                 for item in results])
 
-    # ans = []
-    # if not results:
-    #     ans.append(await builder.article('No videos found for this query.', text='Empty result'))
-    # else:
-    #     for entry in results:
-    #         if entry['id']['kind'] == 'youtube#video':
-    #             ans.append(
-    #                 await builder.article(entry['snippet']['title'], description=entry['snippet']['channelTitle'],
-    #                                       thumb=InputWebDocument(url=entry['snippet']['thumbnails']['high']['url'],
-    #                                                              size=0,
-    #                                                              mime_type="image/jpeg",
-    #                                                              attributes=[], ),
-    #                                       text='https://www.youtube.com/watch?v=' + entry['id']['videoId']))
-    # await event.answer(ans)
-
 
 @client.on(events.NewMessage())
 async def answer(event):
     if event.text == '/start':
-        await event.reply('Siema mały kurwiu ;)')
+        await event.reply('Siema ;)')
     elif event.text.startswith('@'):
         await event.respond("It's me 😂")
     elif 'https://www.youtube.com/' in event.text or 'https://youtu.be/' in event.text:
@@ -130,23 +119,6 @@ async def answer(event):
             try:
                 yt = YouTube(event.text)
                 await yt_download(event, yt)
-                # video = io.BytesIO()
-                # print('Downloading')
-                # stream = yt.streams.first()
-                # type_ = None
-                # for stream in yt.streams:
-                #     if stream.audio_codec == 'opus':
-                #         type_ = stream.mime_type.replace('audio/', '')
-                #         break
-                #     else:
-                #         pass
-                # messId = await event.reply(f'__Downloading {yt.title}__')
-                # yt.streams.get_audio_only(subtype=type_).stream_to_buffer(buffer=video)
-                # file = io.BytesIO(video.getvalue())
-                # file.name = f'{yt.title}.{type_}'
-                # await client.edit_message(messId, f'__Uploading {yt.title}__')
-                # await event.reply(file=file)
-                # await client.delete_messages(event.sender_id, messId)
             except exceptions.VideoUnavailable as e:
                 await event.reply('**Video is unavailable**')
         else:
@@ -155,15 +127,8 @@ async def answer(event):
                 for vid in playlist.videos:
                     await yt_download(event, vid)
             except Exception as e:
+                await event.reply('**Playlist or video is unavailable**')
                 print(e)
-                # messId = await event.reply("Downloading mp4")
-                # video = io.BytesIO()
-                # print('Downloading')
-                # vid.streams.get_audio_only().stream_to_buffer(buffer=video)
-                # file = io.BytesIO(video.getvalue())
-                # file.name = f'{vid.title}.mp4'
-                # print('Uploading')
-                # await client.send_file(event.sender.id, file=file)
 
 
 with client:
