081258431dd3e8ae39029e006a4d5840e905c2a9
Mrkazik99 <mateuszkazmierczak99@gmail.com>
2021-04-07 00:26:38 +0200

    Tagging files, performance improvements, code reformat.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import logging
-import ffmpeg as ffmpeg
-import yaml
 import io
-import aiohttp as aiohttp
-from mutagen import oggopus
+import logging
+import aiohttp
+import ffmpeg
+import yaml
+from mutagen import oggvorbis
 from pytube import YouTube, Playlist, exceptions
 from telethon import TelegramClient, events
 from telethon.tl.types import InputWebDocument
@@ -24,17 +24,28 @@ session = aiohttp.ClientSession()
 f.close()
 
 
-async def youtube_search(type, search_query, amount):
+async def youtube_search(search_type, search_query, amount) -> list or None:
     search_query = search_query.strip()
     if not search_query:
         return
     async with session.get('https://www.googleapis.com/youtube/v3/search', params={'part': 'snippet',
                                                                                    'q': search_query,
                                                                                    'maxResult': amount,
-                                                                                   'type': type,
+                                                                                   'type': search_type,
                                                                                    'key': api_key}) as response:
         resp = await response.json()
         return resp['items']
+
+
+async def generate_names(yt: YouTube):
+    if yt.metadata.metadata and len(yt.metadata.metadata) == 1:
+        return {'Artist': yt.metadata.metadata[0]['Artist'], 'Title': yt.metadata.metadata[0]['Song']}
+        # return {'Artist': yt.metadata.metadata[0]['Artist'], 'Title': yt.metadata.metadata[0]['Song'],
+                # 'Album': yt.metadata.metadata[0]['Album']}
+    #TODO:Check if metadata contains Album
+    else:
+        return {'Artist': yt.author, 'Title': yt.title}
+        # return {'Artist': yt.author, 'Title': yt.title, 'Album': ''}
 
 
 async def yt_download(event: events.newmessage.NewMessage.Event, yt: YouTube) -> None:
@@ -45,73 +56,68 @@ async def yt_download(event: events.newmessage.NewMessage.Event, yt: YouTube) ->
             type_ = stream.mime_type.replace('audio/', '')
             break
         else:
-            pass
+            type_ = 'mp4'
     mess_id = await event.reply(f'__Downloading {yt.title}__')
     yt.streams.get_audio_only(subtype=type_).stream_to_buffer(buffer=video)
     file = io.BytesIO(video.getvalue())
-    file.name = f'{yt.title}.{type_}'
     process = (
         ffmpeg
             .input('pipe:')
-            .output('pipe:', format='opus')
+            .output('pipe:', format='ogg')
             .run_async(pipe_stdin=True, pipe_stdout=True, pipe_stderr=True)
     )
     out, err = process.communicate(input=file.read())
     out_file = io.BytesIO(out)
-    mutagen_file = oggopus.OggOpus(fileobj=out_file)
-    mutagen_file['artist'] = yt.author
-    mutagen_file['title'] = f'!{yt.title}'
+    mutagen_file = oggvorbis.OggVorbis(out_file)
+    metas = await generate_names(yt)
+    mutagen_file['Artist'] = metas['Artist']
+    mutagen_file['Title'] = metas['Title']
+    # mutagen_file['Album'] = metas['Album']
+    #TODO:Add some cover
     mutagen_file.save(out_file)
     out_file = io.BytesIO(out_file.getvalue())
-    out_file.name = f'{yt.title}.opus'
+    #TODO:Try to simplify BytesIO casting
+    out_file.name = f"{metas['Artist']} - {metas['Title']}.ogg"
     await client.edit_message(mess_id, f'__Uploading {yt.title}__')
-    await event.reply(file=out_file)
+    async with client.action(event.sender_id, 'file'):
+        await event.reply(file=out_file)
     await client.delete_messages(event.sender_id, mess_id)
+
+
+async def build_answer(switch: str, event: events.InlineQuery.Event):
+    return [event.builder.article(title=result['snippet']['title'],
+                                  description=f"Published by: {result['snippet']['channelTitle']}",
+                                  thumb=InputWebDocument(
+                                      url=result['snippet']['thumbnails']['default']['url'],
+                                      size=0,
+                                      mime_type='image/jpg',
+                                      attributes=[]),
+                                  text=f"https://www.youtube.com/playlist?list={result['id']['playlistId']}"
+                                  if switch == '.p' else f"https://www.youtube.com/watch?v={result['id']['videoId']}")
+            for result in
+            await youtube_search(search_type='video' if switch == '.v' else 'playlist', search_query=event.text[2:],
+                                 amount=10)]
 
 
 @client.on(events.InlineQuery())
 async def inline_query(event):
-    builder = event.builder
     if not event.text or len(event.text) < 3:
-        ans = builder.article('Wrong command', text='Usage: @YTmusicRipperBot <.p/.v> <video or playlist query>')
-        await event.answer([ans])
+        await event.answer(
+            [event.builder.article('Wrong command', text='Usage: @YTmusicRipperBot <.p/.v> <video or playlist query>')])
         return
     else:
         if event.text.startswith('.p'):
-            results = await youtube_search(type='playlist', search_query=event.text[2:], amount=10)
-            await event.answer([event.builder.article(title=item['snippet']['title'],
-                                                      description=f"Published by: {item['snippet']['channelTitle']}",
-                                                      thumb=InputWebDocument(
-                                                          url=item['snippet']['thumbnails']['default']['url'],
-                                                          size=0,
-                                                          mime_type='image/jpg',
-                                                          attributes=[]),
-                                                      text=f"https://www.youtube.com/playlist?list={item['id']['playlistId']}")
-                                for item in results])
+            await event.answer(await build_answer('.p', event=event))
         elif event.text.startswith('.v'):
-            results = await youtube_search(type='video', search_query=event.text[2:], amount=10)
-            await event.answer([event.builder.article(title=item['snippet']['title'],
-                                                      description=f"Published by: {item['snippet']['channelTitle']}",
-                                                      thumb=InputWebDocument(
-                                                          url=item['snippet']['thumbnails']['default']['url'],
-                                                          size=0, mime_type='image/jpeg', attributes=[]),
-                                                      text=f"https://www.youtube.com/watch?v={item['id']['videoId']}")
-                                for item in results])
+            await event.answer(await build_answer('.v', event=event))
         else:
-            results = await youtube_search(type='video', search_query=event.text, amount=10)
-            await event.answer([event.builder.article(title=item['snippet']['title'],
-                                                      description=f"Published by: {item['snippet']['channelTitle']}",
-                                                      thumb=InputWebDocument(
-                                                          url=item['snippet']['thumbnails']['default']['url'],
-                                                          size=0, mime_type='image/jpeg', attributes=[]),
-                                                      text=f"https://www.youtube.com/watch?v={item['id']['videoId']}")
-                                for item in results])
+            await event.answer(await build_answer('.v', event=event))
 
 
 @client.on(events.NewMessage())
 async def answer(event):
     if event.text == '/start':
-        await event.reply('Siema ;)')
+        await event.reply('Usage: @YTmusicRipperBot <.p/.v> <video or playlist query>')
     elif event.text.startswith('@'):
         await event.respond("It's me 😂")
     elif 'https://www.youtube.com/' in event.text or 'https://youtu.be/' in event.text:
